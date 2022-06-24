7ef8f4c330b1fe600de9b99f9d2fc97e41de2971
Daryl Manning <daryl@wakatara.com>
2022-06-25 02:26:43 +0800

    Sum All Tails safely and tests.

--- slices/sum.go
+++ slices/sum.go
@@ -15,3 +15,15 @@ func SumAll(slices [][]int) []int {
 	}
 	return total
 }
+
+func SumAllTails(slices [][]int) []int {
+	total := []int{}
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			total = append(total, 0)
+		} else {
+			total = append(total, Sum(slice[1:]))
+		}
+	}
+	return total
+}
