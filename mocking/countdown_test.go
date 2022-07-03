e7d1ea320930e2548002c3eef7e00faec0fe321d
Daryl Manning <daryl@wakatara.com>
2022-07-03 19:58:56 +0800

    [WIP] Mocks and dependency injection on countdown.

---
+++ mocking/countdown_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func Test_Countdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
---
+++ mocking/go.mod
@@ -0,0 +1,3 @@
+module github.com/wakatara/learn-go-with-tests/mocking
+
+go 1.18
