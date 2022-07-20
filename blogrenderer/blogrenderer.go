451234e6e05e940fe25f636d77bd1a5f21fcf2db
Daryl Manning <daryl@wakatara.com>
2022-07-20 16:07:54 +0800

    [WIP] Introduce Posts view model to ease testing.

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 type Post struct {
@@ -11,6 +12,15 @@ type Post struct {
 	Description string
 	Tags        []string
 	Body        string
+}
+
+type PostViewModel struct {
+	Title, SanitisedTitle, Description, Body string
+	Tags                                     []string
+}
+
+type PostRenderer struct {
+	templ *template.Template
 }
 
 var (
@@ -25,6 +35,43 @@ func Render(w io.Writer, p Post) error {
 	}
 
 	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, posts); err != nil {
 		return err
 	}
 
