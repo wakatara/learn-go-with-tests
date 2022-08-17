c1ab6576ef9cb6f11bce1ad48a75e808e3c08b4f
Daryl Manning <daryl@wakatara.com>
2022-08-17 12:52:45 +0800

    Added in-memory store.

--- application/main.go
+++ application/main.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 )
 
+// Hard code in memory store temporarily as we implement
+// main.go
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":3333", server))
 }
