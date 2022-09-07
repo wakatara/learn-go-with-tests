b6eb1d657fecee5b06d252d24e98111a8dfbe7d9
Daryl Manning <daryl@wakatara.com>
2022-09-07 15:17:54 +0800

    Refacter file opening for common cli and server usage.

--- application/cmd/webserver/main.go
+++ application/cmd/webserver/main.go
@@ -3,28 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
-	poker "github.com/quii/learn-go-with-tests/command-line/v1"
+	poker "github.com/quii/learn-go-with-tests/command-line/v3"
 )
 
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
 	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
+		log.Fatal(err)
 	}
+	defer close()
 
-	store, err := poker.NewFileSystemPlayerStore(db)
-
-	if err != nil {
-		log.Fatalf("problem creating file system player store, %v ", err)
-	}
 	server := poker.NewPlayerServer(store)
 
-	if err := http.ListenAndServe(":3333", server); err != nil {
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
--- application/file_system_store.go
+++ application/file_system_store.go
@@ -29,6 +29,26 @@ func initialisePlayerDBFile(file *os.File) error {
 	return nil
 }
 
+func FileSystemPlayerStoreFromFile(path string) (*FileSystemPlayerStore, func(), error) {
+	db, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem opening %s %v", path, err)
+	}
+
+	closeFunc := func() {
+		db.Close()
+	}
+
+	store, err := NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("problem creating file system player store, %v ", err)
+	}
+
+	return store, closeFunc, nil
+}
+
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 	err := initialisePlayerDBFile(file)
 
